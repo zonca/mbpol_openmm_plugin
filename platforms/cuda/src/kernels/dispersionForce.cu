acb40393eae35e3773e222920027e565251d0a85
Andrea Zonca <code@andreazonca.com>
2015-04-09 16:46:58 -0700

    implemented dispersion computation with no exclusions

--- platforms/cuda/src/kernels/dispersionForce.cu
+++ platforms/cuda/src/kernels/dispersionForce.cu
@@ -1,12 +1,53 @@
+const real nm_to_A = 10.;
+const real kcal_permol_Aminus6_to_kJ_permol_nmminus6 = 4.184e6;
+const real cal2joule = 4.184;
+
 #ifdef USE_CUTOFF
 if (atom1 < NUM_ATOMS && atom2 < NUM_ATOMS && atom1 != atom2 && r2 < CUTOFF_SQUARED) {
 #else
 if (atom1 < NUM_ATOMS && atom2 < NUM_ATOMS && atom1 != atom2) {
 #endif
-    real sigma = 1.3;
-    real epsilon = 2.;
-    real x = sigma/r;
-    real eps = SQRT(2);
-    dEdR += 2 * epsilon*eps*(12*POW(x, 12.0)-6*POW(x, 6.0)) * invR * invR;
-    tempEnergy += 4.0*eps*(POW(x, 12.0)-POW(x, 6.0));
+
+    real c6 = 3.;
+    real d6 = 10.;
+
+    real d6r = r * d6; // r in nm and d6 is 1/nm so units compensate
+    // tang_toennies function
+    int nn = 6;
+
+    real sum = 1.0 + d6r/nn;
+
+    while (--nn != 0)
+        sum = 1.0 + sum*d6r/nn;
+
+    real tt6 = 1.0 - sum*EXP(-d6r);
+
+    if (abs(tt6) < 1.0e-8) {
+
+        real term(1);
+        for (nn = 6; nn != 0; --nn)
+            term *= d6r/nn;
+
+        sum = 0.0;
+        for (nn = 6 + 1; nn < 1000; ++nn) {
+            term *= d6r/nn;
+            sum += term;
+
+            if (abs(term/sum) < 1.0e-8)
+                break;
+        }
+
+        tt6 = sum*std::exp(-d6r);
+    }
+    // end of tang_toennies function
+
+    real e6 = c6*tt6*POW(1/(r * nm_to_A), 6)/kcal_permol_Aminus6_to_kJ_permol_nmminus6;
+
+    real if6 = 1.0/720; // factorial(6)
+
+    dEdR += (6*e6/(r * r * nm_to_A) - 
+      c6/kcal_permol_Aminus6_to_kJ_permol_nmminus6*POW(d6/nm_to_A, 7)*if6*EXP(-d6r)/r) * cal2joule;
+    // factored out 1 nm_to_A factor
+
+    tempEnergy += -e6 * cal2joule;
 }
