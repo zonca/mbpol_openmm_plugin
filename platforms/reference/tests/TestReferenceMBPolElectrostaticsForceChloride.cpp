71bc6d71ae4b1b4bd425cadc1a0dd52503a23c66
Andrea Zonca <code@andreazonca.com>
2015-06-15 12:11:40 -0700

    support for testing with no charge redistribution

--- platforms/reference/tests/TestReferenceMBPolElectrostaticsForceChloride.cpp
+++ platforms/reference/tests/TestReferenceMBPolElectrostaticsForceChloride.cpp
@@ -65,7 +65,8 @@ static void testWater3VirtualSite( ) {
     std::string testName      = "testWater3VirtualSiteChloride";
     std::cout << "Test START: " << testName << std::endl;
 
-    int numberOfParticles     = 12+1;
+    int particlesPerMolecule = 4;
+    int numberOfParticles     = particlesPerMolecule*3+1;
     double cutoff             = 0.70;
 
     std::vector<double> outputElectrostaticsMoments;
@@ -93,16 +94,21 @@ static void testWater3VirtualSite( ) {
     //mbpolElectrostaticsForce->setMutualInducedMaxIterations( 500 );
     //mbpolElectrostaticsForce->setAEwald( 5.4459052e+00 );
     //mbpolElectrostaticsForce->setEwaldErrorTolerance( 1.0e-04 );
+    if (particlesPerMolecule == 3)
+    	mbpolElectrostaticsForce->setIncludeChargeRedistribution(false);
 
     double virtualSiteWeightO = 0.573293118;
     double virtualSiteWeightH = 0.213353441;
-    for( unsigned int jj = 0; jj < numberOfParticles-1; jj += 4 ){
+    for( unsigned int jj = 0; jj < numberOfParticles-1; jj += particlesPerMolecule){
         system.addParticle( 1.5999000e+01 );
         system.addParticle( 1.0080000e+00 );
         system.addParticle( 1.0080000e+00 );
+
+        if (particlesPerMolecule > 3) {
         system.addParticle( 0. ); // Virtual Site
         system.setVirtualSite(jj+3, new ThreeParticleAverageSite(jj, jj+1, jj+2,
                                                            virtualSiteWeightO, virtualSiteWeightH,virtualSiteWeightH));
+        }
 
     }
     system.addParticle( 35.5 );
@@ -115,15 +121,17 @@ static void testWater3VirtualSite( ) {
     thole[TDDOH]  = 0.626;
     thole[TDDHH] = 0.055;
 
-    for( unsigned int jj = 0; jj < numberOfParticles-1; jj += 4 ){
+    for( unsigned int jj = 0; jj < numberOfParticles-1; jj += particlesPerMolecule ){
         mbpolElectrostaticsForce->addElectrostatics( -5.1966000e-01, jj+1, jj+2, jj+3,
                                             thole, 0.001310, 0.001310 );
         mbpolElectrostaticsForce->addElectrostatics(  2.5983000e-01, jj, jj+2, jj+3,
                                             thole, 0.000294, 0.000294 );
         mbpolElectrostaticsForce->addElectrostatics(  2.5983000e-01, jj, jj+1, jj+3,
                                             thole, 0.000294, 0.000294 );
-        mbpolElectrostaticsForce->addElectrostatics(  0., jj, jj+1, jj+2,
+        if (particlesPerMolecule > 3) {
+        	mbpolElectrostaticsForce->addElectrostatics(  0., jj, jj+1, jj+2,
                                                     thole,  0.001310,  0.);
+        }
     }
     mbpolElectrostaticsForce->addElectrostatics(  -1., 0,0,0,
                                                 thole,  0.0053602, 0.0053602);
@@ -133,23 +141,29 @@ static void testWater3VirtualSite( ) {
     static std::vector<Vec3> positions; // Static to work around bug in Visual Studio that makes compilation very very slow.
     positions.resize(numberOfParticles);
 
-    positions[0]             = Vec3( -1.516074336e+00, -2.023167650e-01,  1.454672917e+00  );
-    positions[1]             = Vec3( -6.218989773e-01, -6.009430735e-01,  1.572437625e+00  );
-    positions[2]             = Vec3( -2.017613812e+00, -4.190350349e-01,  2.239642849e+00  );
-    positions[3]             = Vec3( -1.43230412, -0.33360265,  1.64727446 );
+    int i = 0;
+    positions[i]             = Vec3( -1.516074336e+00, -2.023167650e-01,  1.454672917e+00  ); i++;
+    positions[i]             = Vec3( -6.218989773e-01, -6.009430735e-01,  1.572437625e+00  ); i++;
+    positions[i]             = Vec3( -2.017613812e+00, -4.190350349e-01,  2.239642849e+00  ); i++;
+
+    if (particlesPerMolecule==4){
+    	positions[i]             = Vec3( -1.43230412, -0.33360265,  1.64727446 ); i++;
+    }
 
 
-    positions[4]             = Vec3( -1.763651687e+00, -3.816594649e-01, -1.300353949e+00  );
-    positions[5]             = Vec3( -1.903851736e+00, -4.935677617e-01, -3.457810126e-01  );
-    positions[6]             = Vec3( -2.527904158e+00, -7.613550077e-01, -1.733803676e+00  );
-    positions[7]             = Vec3( -1.95661974, -0.48654484, -1.18917052 );
-
-
-    positions[8]             = Vec3( -5.588472140e-01,  2.006699172e+00, -1.392786582e-01  );
-    positions[9]             = Vec3( -9.411558180e-01,  1.541226676e+00,  6.163293071e-01  );
-    positions[10]            = Vec3( -9.858551734e-01,  1.567124294e+00, -8.830970941e-01  );
-    positions[11]            = Vec3( -0.73151769,  1.8136042 , -0.13676332 );
-    positions[12]            = Vec3( 0,0,0 );
+    positions[i]             = Vec3( -1.763651687e+00, -3.816594649e-01, -1.300353949e+00  ); i++;
+    positions[i]             = Vec3( -1.903851736e+00, -4.935677617e-01, -3.457810126e-01  ); i++;
+    positions[i]             = Vec3( -2.527904158e+00, -7.613550077e-01, -1.733803676e+00  ); i++;
+    if (particlesPerMolecule==4){
+    	positions[i]             = Vec3( -1.95661974, -0.48654484, -1.18917052 ); i++;
+    }
+    positions[i]             = Vec3( -5.588472140e-01,  2.006699172e+00, -1.392786582e-01  ); i++;
+    positions[i]             = Vec3( -9.411558180e-01,  1.541226676e+00,  6.163293071e-01  ); i++;
+    positions[i]            = Vec3( -9.858551734e-01,  1.567124294e+00, -8.830970941e-01  ); i++;
+    if (particlesPerMolecule==4){
+    	positions[i]            = Vec3( -0.73151769,  1.8136042 , -0.13676332 ); i++;
+    }
+    positions[i]            = Vec3( 0,0,0 ); i++;
 
     for (int i=0; i<numberOfParticles; i++) {
         for (int j=0; j<3; j++) {
@@ -181,22 +195,29 @@ static void testWater3VirtualSite( ) {
     std::cout << "Expected energy: " << expectedEnergy/cal2joule << " Kcal/mol "<< std::endl;
 
     std::vector<Vec3> expectedForces(numberOfParticles);
-    expectedForces[0]         = Vec3(  2.613683596e+01,  5.408275264e+00, -1.629517609e+01  );
-    expectedForces[1]         = Vec3( -2.216918164e+01, -2.590793612e+01,  6.296385402e+01  );
-    expectedForces[2]         = Vec3(  1.517701844e+00, -3.046093356e+00,  3.837297571e+00  );
-    expectedForces[4]         = Vec3(  1.636350854e+01, -4.336106706e+00,  1.972968278e+01  );
-    expectedForces[5]         = Vec3( -3.640857280e+01, -8.181611462e+00, -2.800077880e+01  );
-    expectedForces[6]         = Vec3( -1.656236514e+00, -1.105120684e+00,  7.059863729e-01  );
-    expectedForces[8]         = Vec3(  9.977840973e+00, -3.226637538e+01,  8.605903697e+00  );
-    expectedForces[9]         = Vec3( -1.536072074e+01,  3.348477786e+01,  8.224734193e+00  );
-    expectedForces[10]        = Vec3( -2.036848206e+00,  1.324057610e+01, -9.839995796e+00  );
-    expectedForces[12]        = Vec3(  2.363567258e+01,  2.270961449e+01, -4.993150796e+01  );
+    i = 0;
+    expectedForces[i]         = Vec3(  2.613683596e+01,  5.408275264e+00, -1.629517609e+01  ); i++;
+    expectedForces[i]         = Vec3( -2.216918164e+01, -2.590793612e+01,  6.296385402e+01  ); i++;
+    expectedForces[i]         = Vec3(  1.517701844e+00, -3.046093356e+00,  3.837297571e+00  ); i++;
+    if (particlesPerMolecule > 3)
+        i++;
+    expectedForces[i]         = Vec3(  1.636350854e+01, -4.336106706e+00,  1.972968278e+01  ); i++;
+    expectedForces[i]         = Vec3( -3.640857280e+01, -8.181611462e+00, -2.800077880e+01  ); i++;
+    expectedForces[i]         = Vec3( -1.656236514e+00, -1.105120684e+00,  7.059863729e-01  ); i++;
+    if (particlesPerMolecule > 3)
+        i++;
+    expectedForces[i]         = Vec3(  9.977840973e+00, -3.226637538e+01,  8.605903697e+00  ); i++;
+    expectedForces[i]         = Vec3( -1.536072074e+01,  3.348477786e+01,  8.224734193e+00  ); i++;
+    expectedForces[i]        = Vec3( -2.036848206e+00,  1.324057610e+01, -9.839995796e+00  ); i++;
+    if (particlesPerMolecule > 3)
+        i++;
+    expectedForces[i]        = Vec3(  2.363567258e+01,  2.270961449e+01, -4.993150796e+01  ); i++;
 
     // gradient -> forces
     for (int i=0; i<numberOfParticles; i++) {
            for (int j=0; j<3; j++) {
             forces[i][j] /= cal2joule*10;
-            if ((i+1) % 4 == 0) { // Set virtual site force to 0
+            if ((particlesPerMolecule>3) && ((i+1) % 4 == 0)) { // Set virtual site force to 0
                 forces[i][j] = 0;
             }
            }
