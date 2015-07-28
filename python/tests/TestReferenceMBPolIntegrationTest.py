0be74c2e7e7f953c766f015752ff046ae285b039
Sebastian <sqamara@ucsd.edu>
2015-07-28 10:09:52 -0700

    added new refrence tests and pdb files

--- python/tests/TestReferenceMBPolIntegrationTest.py
+++ python/tests/TestReferenceMBPolIntegrationTest.py
@@ -9,20 +9,76 @@ import mbpol
 
 class TestReferenceMBPolIntegration(unittest.TestCase):
     
-    def testMBPolIntegrationTest(self):
+#    def test_water3(self, nonbondedMethod=app.CutoffNonPeriodic):
+#        expected_energy = -8.78893485
+#        pdb = app.PDBFile("../water3.pdb")
+#        forcefield = app.ForceField("../mbpol.xml")
+#        nonbondedCutoff = 0.9*unit.nanometers
+#        
+#        if (nonbondedMethod == app.PME):
+#            expected_energy = -8.92353
+#            boxDimension = 1.9
+#            boxsize = [boxDimension, boxDimension, boxDimension]
+#            pdb.topology.setUnitCellDimensions( boxsize )
+#        system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff)
+#            
+#        integrator = mm.LangevinIntegrator(0.0, 0.1, 0.01)
+#        platform = mm.Platform.getPlatformByName('Reference')
+#        simulation = app.Simulation(pdb.topology, system, integrator, platform)
+#        simulation.context.setPositions(pdb.positions)
+#        simulation.context.computeVirtualSites()
+#        state = simulation.context.getState(getForces=True, getEnergy=True, getPositions=True)
+#        potential_energy = state.getPotentialEnergy()
+#        potential_energy.in_units_of(unit.kilocalorie_per_mole)
+#        
+#        print(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value)
+#        self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < .1)
+#    
+#    def test_water3_periodic(self):
+#        self.test_water3(nonbondedMethod=app.PME)
+#        
+#    def test_water50_periodic(self):
+#        nonbondedMethod=app.PME
+#        expected_energy = -244.37507
+#        pdb = app.PDBFile("pdb_files/water50.pdb")
+#        forcefield = app.ForceField("../mbpol.xml")
+#        nonbondedCutoff = 0.9*unit.nanometer
+#        
+#        if (nonbondedMethod == app.PME):
+#            boxDimension = 1.8
+#            boxsize = [boxDimension, boxDimension, boxDimension]
+#            pdb.topology.setUnitCellDimensions( boxsize )
+#            
+#        system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff)
+#            
+#        integrator = mm.LangevinIntegrator(0.0, 0.1, 0.01)
+#        platform = mm.Platform.getPlatformByName('Reference')
+#        simulation = app.Simulation(pdb.topology, system, integrator, platform)
+#        simulation.context.setPositions(pdb.positions)
+#        simulation.context.computeVirtualSites()
+#        state = simulation.context.getState(getForces=True, getEnergy=True, getPositions=True)
+#        potential_energy = state.getPotentialEnergy()
+#        potential_energy.in_units_of(unit.kilocalorie_per_mole)
+#        
+#        print(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value)
+#        self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < 1)
+#        
+   def test_water256_periodic(self):
+        nonbondedMethod=app.PME
         expected_energy = -2270.88890
-        tolerance = 1.0e-2
-        pdb = app.PDBFile("../water256_integration_test.pdb")
+        pdb = app.PDBFile("pdb_files/water256_integration_test.pdb")
         forcefield = app.ForceField("../mbpol.xml")
-        nonbondedCutoff = .09
-        nonbondedMethod = app.CutoffPeriodic
-        #if (nonbondedMethod == app.CutoffPeriodic):
-        boxsize = [19.3996888399961804/10., 19.3996888399961804/10., 19.3996888399961804/10.]
-        pdb.topology.setUnitCellDimensions( boxsize )
-        system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod,
-                                         nonBondedCutoff=nonbondedCutoff)
+
+        nonbondedCutoff = 0.9*unit.nanometer
+        
+        if (nonbondedMethod == app.PME):
+            boxDimension = 19.3996888399961804/10.
+            boxsize = [boxDimension, boxDimension, boxDimension]
+            pdb.topology.setUnitCellDimensions( boxsize )
             
-        integrator = mm.LangevinIntegrator(0.02*unit.femtoseconds)
+        system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff)
+            
+        integrator = mm.LangevinIntegrator(0.0, 0.1, 0.01)
         platform = mm.Platform.getPlatformByName('Reference')
         simulation = app.Simulation(pdb.topology, system, integrator, platform)
         simulation.context.setPositions(pdb.positions)
@@ -30,9 +86,11 @@ class TestReferenceMBPolIntegration(unittest.TestCase):
         state = simulation.context.getState(getForces=True, getEnergy=True, getPositions=True)
         potential_energy = state.getPotentialEnergy()
         potential_energy.in_units_of(unit.kilocalorie_per_mole)
+        
         print(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value)
-        self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value -expected_energy) < tolerance)
         
         
+        self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < 1)
+
 if __name__ == '__main__':
     unittest.main()
