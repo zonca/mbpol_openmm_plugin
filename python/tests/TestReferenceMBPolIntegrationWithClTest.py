310f885166c8dfd606f2fef0c6ce5c685139e7a7
Sebastian <sqamara@ucsd.edu>
2015-09-01 11:03:36 -0700

    made integration test PDB and test outline, waiting for energy and forces for this configuration

---
+++ python/tests/TestReferenceMBPolIntegrationWithClTest.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import unittest
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit
+import sys
+import mbpol
+
+class TestReferenceMBPolIntegrationWithCl(unittest.TestCase):
+
+    def test_water3andCl(self):
+        expected_energy = -TBD
+        nonbondedMethod = app.CutoffNonPeriodic
+        pdb = app.PDBFile("pdb_files/water3AndCl.pdb")
+        forcefield = app.ForceField("../mbpol.xml")
+        nonbondedCutoff = 10*unit.nanometers
+        system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=nonbondedCutoff)
+        integrator = mm.LangevinIntegrator(0.0, 0.1, 0.01)
+        platform = mm.Platform.getPlatformByName('Reference')
+        
+        simulation = app.Simulation(pdb.topology, system, integrator, platform)
+        simulation.context.setPositions(pdb.positions)
+        simulation.context.computeVirtualSites()
+       
+        state = simulation.context.getState(getForces=True, getEnergy=True, getPositions=True)
+        potential_energy = state.getPotentialEnergy()
+        potential_energy.in_units_of(unit.kilocalorie_per_mole)
+
+    #print(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value)
+    self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < .1)
