c19f41b468c1d0dd60540af4e47a71b4ccc72b5a
Sebastian <sqamara@ucsd.edu>
2015-11-09 15:39:35 -0800

    made cuda test match the reference test

--- platforms/cuda/tests/TestCudaMBPolThreeBodyForce.cpp
+++ platforms/cuda/tests/TestCudaMBPolThreeBodyForce.cpp
@@ -30,7 +30,7 @@
  * -------------------------------------------------------------------------- */
 
 /**
- * This tests the Reference implementation of MBPolOneBodyForce.
+ * This tests the Cuda implementation of MBPolOneBodyForce.
  */
 
 #include "OpenMMMBPol.h"
@@ -59,25 +59,25 @@ extern "C" OPENMM_EXPORT void registerMBPolCudaKernelFactories();
 
 void testThreeBody( double boxDimension, bool addPositionOffset ) {
 
-    std::string testName      = "testMBPol2BodyInteraction";
+    std::string testName      = "testMBPolThreeBodyInteraction";
 
     System system;
-    int numberOfParticles          = 6;
+    int numberOfParticles          = 9;
     MBPolThreeBodyForce* mbpolThreeBodyForce = new MBPolThreeBodyForce();
     double cutoff = 10;
     mbpolThreeBodyForce->setCutoff( cutoff );
 
-    unsigned int particlesPerMolecule = 3;
-
     if( boxDimension > 0.0 ){
-        Vec3 a( boxDimension, 0.0, 0.0 );
-        Vec3 b( 0.0, boxDimension, 0.0 );
-        Vec3 c( 0.0, 0.0, boxDimension );
-        system.setDefaultPeriodicBoxVectors( a, b, c );
-        mbpolThreeBodyForce->setNonbondedMethod(MBPolThreeBodyForce::CutoffPeriodic);
-    } else {
-        mbpolThreeBodyForce->setNonbondedMethod(MBPolThreeBodyForce::CutoffNonPeriodic);
-    }
+            Vec3 a( boxDimension, 0.0, 0.0 );
+            Vec3 b( 0.0, boxDimension, 0.0 );
+            Vec3 c( 0.0, 0.0, boxDimension );
+            system.setDefaultPeriodicBoxVectors( a, b, c );
+            mbpolThreeBodyForce->setNonbondedMethod(MBPolThreeBodyForce::CutoffPeriodic);
+        } else {
+            mbpolThreeBodyForce->setNonbondedMethod(MBPolThreeBodyForce::CutoffNonPeriodic);
+        }
+
+    unsigned int particlesPerMolecule = 3;
 
     std::vector<int> particleIndices(particlesPerMolecule);
     for( unsigned int jj = 0; jj < numberOfParticles; jj += particlesPerMolecule ){
@@ -105,6 +105,10 @@ void testThreeBody( double boxDimension, bool addPositionOffset ) {
     positions[4]             = Vec3( -1.903851736e+00, -4.935677617e-01, -3.457810126e-01  );
     positions[5]             = Vec3( -2.527904158e+00, -7.613550077e-01, -1.733803676e+00  );
 
+    positions[6]             = Vec3( -5.588472140e-01,  2.006699172e+00, -1.392786582e-01  );
+    positions[7]             = Vec3( -9.411558180e-01,  1.541226676e+00,  6.163293071e-01  );
+    positions[8]             = Vec3( -9.858551734e-01,  1.567124294e+00, -8.830970941e-01  );
+
     for (int i=0; i<numberOfParticles; i++) {
         for (int j=0; j<3; j++) {
             positions[i][j] *= 1e-1;
@@ -113,19 +117,28 @@ void testThreeBody( double boxDimension, bool addPositionOffset ) {
 
     if (addPositionOffset) {
         // move second molecule 1 box dimension in Y direction
-        // positions[3][1] += boxDimension;
-        // positions[4][1] += boxDimension;
+        positions[3][1] += boxDimension;
+        positions[4][1] += boxDimension;
         positions[5][1] += boxDimension;
     }
 
-    expectedForces[0]     = Vec3( -4.85337479, -4.47836379 ,-20.08989563);
-    expectedForces[1]     = Vec3( -0.31239868,  0.52518586 , -1.88893830);
-    expectedForces[2]     = Vec3(  0.00886712,  0.73323536 , -1.81715325);
-    expectedForces[3]     = Vec3( -0.65181727, -0.72947395 ,  5.88973293);
-    expectedForces[4]     = Vec3(  4.82340981,  3.20090213 , 16.49522051);
-    expectedForces[5]     = Vec3(  0.98531382,  0.74851439 ,  1.41103374);
+    expectedForces[0]     = Vec3(  0.29919011, -0.34960381, -0.16238472 );
+    expectedForces[1]     = Vec3(  0.34138467, -0.01255068, -0.00998383 );
+    expectedForces[2]     = Vec3( -0.44376649,  0.03687577,  0.54604510 );
+    expectedForces[3]     = Vec3( -0.01094164, -0.36171476, -0.05130395 );
+    expectedForces[4]     = Vec3(  0.24939202,  1.29382952,  0.22930712 );
+    expectedForces[5]     = Vec3( -0.13250943, -0.19313418, -0.34123592 );
+    expectedForces[6]     = Vec3(  0.56722869,  0.46036139, -0.39999973 );
+    expectedForces[7]     = Vec3( -0.75669111, -0.76132457, -0.29799486 );
+    expectedForces[8]     = Vec3( -0.11328682, -0.11273867,  0.48755080 );
 
-    expectedEnergy        = 6.14207815;
+
+    // gradients => forces
+    for( unsigned int ii = 0; ii < expectedForces.size(); ii++ ){
+        expectedForces[ii] *= -1;
+    }
+
+    expectedEnergy        = 0.15586446;
 
     system.addForce(mbpolThreeBodyForce);
     std::string platformName;
@@ -140,8 +153,9 @@ void testThreeBody( double boxDimension, bool addPositionOffset ) {
     std::vector<Vec3> forces         = state.getForces();
 
     for( unsigned int ii = 0; ii < forces.size(); ii++ ){
-        forces[ii] /= CalToJoule*10;
-        expectedForces[ii] *= -1; // gradient -> forces
+        forces[ii][0] /= CalToJoule*10;
+        forces[ii][1] /= CalToJoule*10;
+        forces[ii][2] /= CalToJoule*10;
     }
 
     double tolerance = 1.0e-03;
@@ -151,27 +165,24 @@ void testThreeBody( double boxDimension, bool addPositionOffset ) {
 
     std::cout << "Energy: " << energy << " Kcal/mol "<< std::endl;
     std::cout << "Expected energy: " << expectedEnergy << " Kcal/mol "<< std::endl;
-    std::cout << "Ratio: " << energy / expectedEnergy * 100 << " % "<< std::endl;
 
     std::cout  << std::endl << "Forces:" << std::endl;
 
     for (int i=0; i<numberOfParticles; i++) {
            std::cout << "Force atom " << i << ": " << expectedForces[i] << " Kcal/mol/A <mbpol>" << std::endl;
-           std::cout << "Force atom " << i << ": " << forces[i] << " Kcal/mol/A <openmm-mbpol>" << std::endl;
-           std::cout << "Ratio atom" << i << ": [" << forces[i][0] / expectedForces[i][0] * 100 << ", "  << forces[i][1] / expectedForces[i][1] * 100  <<  ", "  << forces[i][2] / expectedForces[i][2] * 100  << "] % " << std::endl << std::endl;
+           std::cout << "Force atom " << i << ": " << forces[i] << " Kcal/mol/A <openmm-mbpol>" << std::endl << std::endl;
        }
 
        std::cout << "Comparison of energy and forces with tolerance: " << tolerance << std::endl << std::endl;
 
 
 
-    ASSERT_EQUAL_TOL( expectedEnergy, energy, tolerance );
+   ASSERT_EQUAL_TOL( expectedEnergy, energy, tolerance );
 
-    for( unsigned int ii = 0; ii < forces.size(); ii++ ){
-        ASSERT_EQUAL_VEC( expectedForces[ii], forces[ii], tolerance );
-    }
-       std::cout << "Test Successful: " << testName << std::endl << std::endl;
-
+   for( unsigned int ii = 0; ii < forces.size(); ii++ ){
+       ASSERT_EQUAL_VEC( expectedForces[ii], forces[ii], tolerance );
+   }
+   std::cout << "Test Successful: " << testName << std::endl << std::endl;
 }
 
 //void testImageMolecules( bool runTestWithAtomImaging, bool addPositionOffset) {
@@ -236,23 +247,24 @@ int main(int argc, char* argv[]) {
         registerMBPolCudaKernelFactories();
         if (argc > 1)
             Platform::getPlatformByName("CUDA").setPropertyDefaultValue("CudaPrecision", string(argv[1]));
-        std::cout << "TestReferenceMBPolThreeBodyForce running test..." << std::endl;
+        std::cout << "TestCudaMBPolThreeBodyForce running test..." << std::endl;
+
 
         double boxDimension = 0;
-        std::cout << "TestReferenceMBPolThreeBodyForce Cluster" << std::endl;
+        std::cout << "TestCudaMBPolThreeBodyForce Cluster" << std::endl;
         testThreeBody( boxDimension, false );
 
-        std::cout << "TestReferenceMBPolThreeBodyForce  Periodic boundary conditions" << std::endl;
+        std::cout << "TestCudaMBPolThreeBodyForce  Periodic boundary conditions" << std::endl;
         boxDimension = 50;
         testThreeBody( boxDimension, false);
 
-        std::cout << "TestReferenceMBPolThreeBodyForce  Periodic boundary conditions with boxDimension offset on second water molecule" << std::endl;
+        std::cout << "TestCudaMBPolThreeBodyForce  Periodic boundary conditions with boxDimension offset on second water molecule" << std::endl;
         boxDimension = 50;
         testThreeBody( boxDimension, true);
 
-    }
-    catch(const std::exception& e) {
+    } catch(const std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
+        std::cout << "FAIL - ERROR.  Test failed." << std::endl;
         return 1;
     }
     std::cout << "Done" << std::endl;
