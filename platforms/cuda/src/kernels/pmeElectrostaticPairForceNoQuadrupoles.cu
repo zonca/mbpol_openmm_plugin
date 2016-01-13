ceaedfe6de5ed7f193d2272db8e018e05a79698f
Andrea Zonca <code@andreazonca.com>
2016-01-12 16:31:29 -0800

    wrote energy computation using real3

--- platforms/cuda/src/kernels/pmeElectrostaticPairForceNoQuadrupoles.cu
+++ platforms/cuda/src/kernels/pmeElectrostaticPairForceNoQuadrupoles.cu
@@ -29,9 +29,7 @@ computeOneInteractionF1(
     real offset = 1-mScale;
     real rr3 = rr1*rr1*rr1;
     real gf4 = 2*(bn2 - 3*offset*rr3*rr1*rr1);
-    real ftm21 = 0;
-    real ftm22 = 0;
-    real ftm23 = 0;
+    real3 ftm2 = make_real3(0);
 
     // calculate the scalar products for permanent components
 
@@ -63,19 +61,16 @@ computeOneInteractionF1(
 
     energy += -forceFactor*(rr1*gl0*(1 - scale1CC));
 
+    real3 delta3 = trimTo3(delta);
     real gf1 = bn1*gl0;
     gf1 -= offset*(rr3*gl0);
-    ftm21 += gf1*xr;
-    ftm22 += gf1*yr;
-    ftm23 += gf1*zr;
+    ftm2 += gf1*delta3;
 
     real gf2 = -ck*bn1 - offset*(-ck*rr3);
 
     real gf3 = ci*bn1 - offset*(ci*rr3);
 
-    force.x = ftm21;
-    force.y = ftm22;
-    force.z = ftm23;
+    force = ftm2;
 }
 
 
@@ -94,6 +89,7 @@ computeOneInteractionF2(
     real xr = delta.x;
     real yr = delta.y;
     real zr = delta.z;
+    real3 delta3 = trimTo3(delta);
     real rr1 = delta.w;
     real psc3 = 1.;
     real dsc3 = 1.;
@@ -110,11 +106,9 @@ computeOneInteractionF2(
     real bn4 = bn.w;
 
     real rr5 = rr1*rr1;
-          rr5 = 3*rr1*rr5*rr5;
+    rr5 = 3*rr1*rr5*rr5;
 
-    real ftm21 = 0;
-    real ftm22 = 0;
-    real ftm23 = 0;
+    real3 ftm2 = make_real3(0);
 
     real rr3 = rr1*rr1*rr1;
 
@@ -122,14 +116,10 @@ computeOneInteractionF2(
 
     real prefactor1;
     prefactor1 = 0.5f;//*(ci*psc3 + sc3*psc5 - gfi3);
-    ftm21 -= prefactor1*atom2.inducedDipole.x;
-    ftm22 -= prefactor1*atom2.inducedDipole.y;
-    ftm23 -= prefactor1*atom2.inducedDipole.z;
+    ftm2 -= prefactor1*atom2.inducedDipole;
 
     prefactor1 = 0.5f;//*(ci*dsc3 + sc3*dsc5 - gfi3);
-    ftm21 -= prefactor1*atom2.inducedDipolePolar.x;
-    ftm22 -= prefactor1*atom2.inducedDipolePolar.y;
-    ftm23 -= prefactor1*atom2.inducedDipolePolar.z;
+    ftm2 -= prefactor1*atom2.inducedDipolePolar;
 
     // RealOpenMM scale3CD =getAndScaleInverseRs(particleI,particleJ,r,true,3,TCD);
 
@@ -153,12 +143,12 @@ computeOneInteractionF2(
     energy += forceFactor*0.5f*sci4*(rr3 * (1 - scale3CD) - bn1)*ci;
 
     real scip4 = atom2.inducedDipolePolar.x*xr + atom2.inducedDipolePolar.y*yr + atom2.inducedDipolePolar.z*zr;
-#ifndef DIRECT_POLARIZATION
-    prefactor1 = 0.5f*(bn2 );
-    ftm21 += prefactor1*((sci4*atom1.inducedDipolePolar.x + scip4*atom1.inducedDipole.x));
-    ftm22 += prefactor1*((sci4*atom1.inducedDipolePolar.y + scip4*atom1.inducedDipole.y));
-    ftm23 += prefactor1*((sci4*atom1.inducedDipolePolar.z + scip4*atom1.inducedDipole.z));
-#endif
+//#ifndef DIRECT_POLARIZATION
+//    prefactor1 = 0.5f*(bn2 );
+//    ftm21 += prefactor1*((sci4*atom1.inducedDipolePolar.x + scip4*atom1.inducedDipole.x));
+//    ftm22 += prefactor1*((sci4*atom1.inducedDipolePolar.y + scip4*atom1.inducedDipole.y));
+//    ftm23 += prefactor1*((sci4*atom1.inducedDipolePolar.z + scip4*atom1.inducedDipole.z));
+//#endif
 
     real gli1 = -ci*sci4;
     real glip1 = -ci*scip4;
@@ -166,9 +156,7 @@ computeOneInteractionF2(
     real gfi1 = (bn2*(gli1+glip1));
     gfi1 -= (rr1*rr1)*(3*(gli1*psc3 + glip1*dsc3));
     gfi1 *= 0.5f;
-    ftm21 += gfi1*xr;
-    ftm22 += gfi1*yr;
-    ftm23 += gfi1*zr;
+    ftm2 += gfi1 * delta3;
 
     {
         real expdamp = EXP(damp);
@@ -176,23 +164,11 @@ computeOneInteractionF2(
         real temp5 = -damp;
         real temp7 = -0.2f - 0.6f*damp;
 
-        real ddsc31 = temp3*xr;
-        real ddsc32 = temp3*yr;
-        real ddsc33 = temp3*zr;
-
-        real ddsc51 = temp5*ddsc31;
-        real ddsc52 = temp5*ddsc32;
-        real ddsc53 = temp5*ddsc33;
-
-        real ddsc71 = temp7*ddsc51;
-        real ddsc72 = temp7*ddsc52;
-        real ddsc73 = temp7*ddsc53;
+        real3 ddsc3 = temp3*delta3;
 
         real rr3 = rr1*rr1*rr1;
         temp3 = (gli1*pScale + glip1*dScale);
-        ftm21 -= rr3*temp3*ddsc31;
-        ftm22 -= rr3*temp3*ddsc32;
-        ftm23 -= rr3*temp3*ddsc33;
+        ftm2 -= rr3*temp3*ddsc3;
     }
 
 //K
@@ -201,14 +177,10 @@ computeOneInteractionF2(
     real gfi2 = (-ck*bn1 );
 
     prefactor1 = 0.5f*(ck*psc3 + gfi2);
-    ftm21 += prefactor1*atom1.inducedDipole.x;
-    ftm22 += prefactor1*atom1.inducedDipole.y;
-    ftm23 += prefactor1*atom1.inducedDipole.z;
+    ftm2 += prefactor1*atom1.inducedDipole;
 
     prefactor1 = 0.5f*(ck*dsc3 + gfi2);
-    ftm21 += prefactor1*atom1.inducedDipolePolar.x;
-    ftm22 += prefactor1*atom1.inducedDipolePolar.y;
-    ftm23 += prefactor1*atom1.inducedDipolePolar.z;
+    ftm2 += prefactor1*atom1.inducedDipolePolar;
 
     real sci3 = atom1.inducedDipole.x*xr + atom1.inducedDipole.y*yr + atom1.inducedDipole.z*zr;
     energy += forceFactor*0.5f*sci3*(ck*(bn1-rr3 * (1 - scale3CD)));
@@ -217,9 +189,7 @@ computeOneInteractionF2(
 #ifndef DIRECT_POLARIZATION
     prefactor1 = 0.5f*(bn2 );
 
-    ftm21 += prefactor1*(sci3*atom2.inducedDipolePolar.x + scip3*atom2.inducedDipole.x);
-    ftm22 += prefactor1*(sci3*atom2.inducedDipolePolar.y + scip3*atom2.inducedDipole.y);
-    ftm23 += prefactor1*(sci3*atom2.inducedDipolePolar.z + scip3*atom2.inducedDipole.z);
+    ftm2 += prefactor1*(sci3*atom2.inducedDipolePolar + scip3*atom2.inducedDipole);
     
     real sci34;
     sci4 = atom2.inducedDipole.x*xr + atom2.inducedDipole.y*yr + atom2.inducedDipole.z*zr;
@@ -227,9 +197,9 @@ computeOneInteractionF2(
     sci34 = (sci3*scip4+scip3*sci4);
 
     gfi1 = sci34*(usc5*(5*rr1*rr1) -bn3);
-#else
-    gfi1 = 0;
-#endif
+//#else
+//    gfi1 = 0;
+//#endif
     
 
     real scip2 = atom1.inducedDipole.x*atom2.inducedDipolePolar.x +
@@ -245,15 +215,13 @@ computeOneInteractionF2(
 
     gfi1 += (bn2*(gli1+glip1));
     gfi1 -= (rr1*rr1)*(3*(gli1*psc3 + glip1*dsc3));
-#ifndef DIRECT_POLARIZATION
+//#ifndef DIRECT_POLARIZATION
     gfi1 += scip2*(bn2 - (3*rr1*rr1)*usc3);
-#endif
+//#endif
     
     gfi1 *= 0.5f;
 
-    ftm21 += gfi1*xr;
-    ftm22 += gfi1*yr;
-    ftm23 += gfi1*zr;
+    ftm2 += gfi1*delta3;
 
     {
         real expdamp = EXP(damp);
@@ -261,36 +229,19 @@ computeOneInteractionF2(
         real temp5 = -damp;
         real temp7 = -0.2f - 0.6f*damp;
 
-        real ddsc31 = temp3*xr;
-        real ddsc32 = temp3*yr;
-        real ddsc33 = temp3*zr;
-
-        real ddsc51 = temp5*ddsc31;
-        real ddsc52 = temp5*ddsc32;
-        real ddsc53 = temp5*ddsc33;
-
-        real ddsc71 = temp7*ddsc51;
-        real ddsc72 = temp7*ddsc52;
-        real ddsc73 = temp7*ddsc53;
+        real3 ddsc3 = temp3*delta3;
 
         real rr3 = rr1*rr1*rr1;
 
         temp3 = gli1*pScale + glip1*dScale;
 
-        ftm21 -= rr3*temp3*ddsc31;
-        ftm22 -= rr3*temp3*ddsc32;
-        ftm23 -= rr3*temp3*ddsc33;
+        ftm2 -= rr3*temp3*ddsc3;
 
 #ifndef DIRECT_POLARIZATION
         temp3 =  uScale*scip2;
-        temp5 = -(3*rr1*rr1)*uScale*sci34;
-        ftm21 -= rr3*(temp3*ddsc31 + temp5*ddsc51);
-        ftm22 -= rr3*(temp3*ddsc32 + temp5*ddsc52);
-        ftm23 -= rr3*(temp3*ddsc33 + temp5*ddsc53);
+        ftm2 -= rr3*(temp3*ddsc3);
 #endif
     }
 
-    force.x += ftm21;
-    force.y += ftm22;
-    force.z += ftm23;
+    force += ftm2;
 }
