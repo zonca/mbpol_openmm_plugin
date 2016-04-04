65149af08fcfeb8aa332ee23f4e77e6e0f40227a
Sebastian <sqamara@ucsd.edu>
2016-04-04 13:33:39 -0700

    added a simple Python test case to verify thole parameters are being set from xml

---
+++ python/check_thole_param.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit
+import sys
+import mbpol
+
+pdb = app.PDBFile("water14_cluster.pdb")
+forcefield = app.ForceField("mbpol.xml")
+#<MBPolElectrostaticsForce thole-charge-charge="0.4" thole-charge-dipole="0.4" thole-dipole-dipole="0.055" thole-dipole-dipole-singlebond="0.626">
+expected = [0.4, 0.4, 0.055, 0.626, 0.055]
+#<MBPolElectrostaticsForce thole-charge-charge="1" thole-charge-dipole="2" thole-dipole-dipole="3" thole-dipole-dipole-singlebond="4">
+#expected = [1, 2, 3, 4, 3]
+i = 0
+for param in forcefield._forces[0].thole:
+    assert ( param == expected[i])
+    i+=1
