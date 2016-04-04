7fb9a54ee9a81dae8110aadd42247c6b9465d69e
Sebastian <sqamara@ucsd.edu>
2016-04-04 16:44:13 -0700

    updated iteration technique in test

--- python/check_thole_param.py
+++ python/check_thole_param.py
@@ -11,7 +11,6 @@ forcefield = app.ForceField("mbpol.xml")
 expected = [0.4, 0.4, 0.055, 0.626, 0.055]
 #<MBPolElectrostaticsForce thole-charge-charge="1" thole-charge-dipole="2" thole-dipole-dipole="3" thole-dipole-dipole-singlebond="4">
 #expected = [1, 2, 3, 4, 3]
-i = 0
-for param in forcefield._forces[0].thole:
-    assert ( param == expected[i])
-    i+=1
+for param, expected_param in zip(forcefield._forces[0].thole, expected):
+    assert ( param == expected_param)
+print ("Test Passed")
