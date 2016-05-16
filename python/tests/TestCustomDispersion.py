c6045c0b9c24b25f1098823714e0000db30320ef
Sebastian <sqamara@ucsd.edu>
2016-05-16 15:04:29 -0700

    updated to use mbpol.xml

--- python/tests/TestCustomDispersion.py
+++ python/tests/TestCustomDispersion.py
@@ -13,14 +13,28 @@ class TestCustomForce(unittest.TestCase):
     def test_three_water(self, nonbondedMethod=app.CutoffNonPeriodic):
         expected_energy = -6.84471477
         pdb = app.PDBFile("../water3.pdb")
-        forcefield = app.ForceField("../customdispersion.xml")
+        forcefield = app.ForceField("../mbpol.xml")
         nonbondedCutoff = 1e3*unit.nanometer
         
         if (nonbondedMethod == app.CutoffPeriodic):
             boxsize = [50, 50, 50]
             pdb.topology.setUnitCellDimensions( boxsize )
         system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonBondedCutoff=nonbondedCutoff)
-            
+        
+        #forces must be removed from the system to test single force
+        #order of forces:
+        # elec
+        # one
+        # two
+        # three
+        # CMMotionRemover
+        # CustomDispersion
+        system.removeForce(0) #remove elec
+        system.removeForce(0) #remove one 
+        system.removeForce(0) #remove two
+        system.removeForce(0) #remove three
+        system.removeForce(0) #remove CMMotionRemover
+           
         integrator = mm.VerletIntegrator(0.02*unit.femtoseconds)
         platform = mm.Platform.getPlatformByName('Reference')
         simulation = app.Simulation(pdb.topology, system, integrator, platform)
@@ -32,17 +46,29 @@ class TestCustomForce(unittest.TestCase):
         
         self.assertTrue(abs(potential_energy.in_units_of(unit.kilocalorie_per_mole)._value - expected_energy) < .01)
     
-    def test_three_water_periodic(self):
-        self.test_three_water(nonbondedMethod=app.CutoffPeriodic)
-        
     def test_water_and_ion(self):
         expected_energy = -1.306598
         pdb = app.PDBFile("pdb_files/water_and_ion.pdb")
-        forcefield = app.ForceField("../customdispersion.xml")
+        forcefield = app.ForceField("../mbpol.xml")
         nonbondedMethod = app.CutoffNonPeriodic
         nonbondedCutoff = 1e3*unit.nanometer
         system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, 
-                                         nonBondedCutoff=nonbondedCutoff)
+                                         nonBondedCutoff=nonbondedCutoff) 
+        
+        #forces must be removed from the system to test single force
+        #order of forces:
+        # elec
+        # one
+        # two
+        # three
+        # CMMotionRemover
+        # CustomDispersion
+        system.removeForce(0) #remove elec
+        system.removeForce(0) #remove one 
+        system.removeForce(0) #remove two
+        system.removeForce(0) #remove three
+        system.removeForce(0) #remove CMMotionRemover
+         
         integrator = mm.VerletIntegrator(0.02*unit.femtoseconds)
         platform = mm.Platform.getPlatformByName('Reference')
         simulation = app.Simulation(pdb.topology, system, integrator, platform)
