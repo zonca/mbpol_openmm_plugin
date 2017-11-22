f67da4a0e50589adeec9836188a86024df483153
Andrea Zonca <code@andreazonca.com>
2017-11-22 10:25:26 -0800

    improved the benchmark script

--- python/utils/run_benchmark.py
+++ python/utils/run_benchmark.py
@@ -7,52 +7,64 @@ import pandas as pd
 import datetime
 import mbpol
 
-configurations = (("amoeba2013", "CUDA"), ("amoeba2013", "Reference"),)#, ("mbpol", "Reference"),)
-configurations = (("mbpol", "Reference"),)
-steps = 10000
+configurations = [("mbpol", "Reference"),("amoeba2013", "Reference")]
+boundaries = ["PME", "cluster"]
+num_molecules_list = [256, 512]
+
+pdb_filenames = { 512: "pdb/H2O_1",
+                  256: "pdb/TIP5P_PIMC_0C_LIQ.0"
+                }
+
+steps = 100
 timestep = 0.02*unit.femtoseconds
 temperature = 300*unit.kelvin
 
-for forcefield_configuration, platform_name in configurations:
+for num_molecules in num_molecules_list:
+    for boundary in boundaries:
+        for forcefield_configuration, platform_name  in configurations:
 
-    pdb_filename = "pdb/TIP5P_PIMC_0C_LIQ"
-    pdb_filename = "pdb/H2O_1"
-    if forcefield_configuration.startswith("amoeba"):
-        pdb_filename += "_AMOEBA"
-    pdb_filename += ".pdb"
-    pdb = app.PDBFile(pdb_filename)
+            pdb_filename = pdb_filenames[num_molecules]
+            if forcefield_configuration.startswith("amoeba"):
+                pdb_filename += "_AMOEBA"
+            pdb_filename += ".pdb"
+            pdb = app.PDBFile(pdb_filename)
 
-    tag = os.path.basename(pdb_filename).replace(".pdb", "")
+            tag = os.path.basename(pdb_filename).replace(".pdb", "")
 
-    # PME box size
-    if tag.startswith("H2O"):
-        boxSize = 24.8343697144598003
-    elif tag.startswith("FM_25C") or tag.startswith("TIP5P_25C"):
-        boxSize = 19.7316565863235596
-    else:
-        boxSize = 19.3996888399961804
-    boxSize /= 10.
-    pdb.topology.setUnitCellDimensions((boxSize,boxSize,boxSize))
+            if boundary == "PME":
+                # PME box size
+                if tag.startswith("H2O"):
+                    boxSize = 24.8343697144598003
+                elif tag.startswith("FM_25C") or tag.startswith("TIP5P_25C"):
+                    boxSize = 19.7316565863235596
+                else:
+                    boxSize = 19.3996888399961804
+                boxSize /= 10.
+                pdb.topology.setUnitCellDimensions((boxSize,boxSize,boxSize))
+                nonbondedMethod = app.PME
 
+            elif boundary == "cluster":
 
-    forcefield = app.ForceField(forcefield_configuration + ".xml")
+                nonbondedMethod = app.CutoffNonPeriodic if forcefield_configuration == "mbpol" else app.NoCutoff
 
-    system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME, nonbondedCutoff=.9*unit.nanometer)
-    integrator = mm.VerletIntegrator(timestep)
+            forcefield = app.ForceField(forcefield_configuration + ".xml")
 
-    platform = mm.Platform.getPlatformByName(platform_name)
-    simulation = app.Simulation(pdb.topology, system, integrator, platform)
-    simulation.context.setPositions(pdb.positions)
-    simulation.context.computeVirtualSites()
+            system = forcefield.createSystem(pdb.topology, nonbondedMethod=nonbondedMethod, nonbondedCutoff=.9*unit.nanometer)
+            integrator = mm.VerletIntegrator(timestep)
 
-    state = simulation.context.getState(getForces=True, getEnergy=True)
-    simulation.context.setVelocitiesToTemperature(temperature)
+            platform = mm.Platform.getPlatformByName(platform_name)
+            simulation = app.Simulation(pdb.topology, system, integrator, platform)
+            simulation.context.setPositions(pdb.positions)
+            simulation.context.computeVirtualSites()
 
-    simulation.reporters.append(app.StateDataReporter("simulation_{tag}_{timestep}_{temperature}.log".format(**locals()).replace(" ",""), 100, step=True, totalEnergy=True,
-    potentialEnergy=True, temperature=True, progress=True, remainingTime=True,
-    speed=True, totalSteps=steps, separator=','))
+            state = simulation.context.getState(getForces=True, getEnergy=True)
+            simulation.context.setVelocitiesToTemperature(temperature)
 
-    start = datetime.datetime.now()
-    simulation.step(steps)
-    end = datetime.datetime.now()
-    print("%s: %s %s time elapsed (%d steps): %f s" % (pdb_filename, forcefield_configuration, platform_name, steps, (end-start).total_seconds()))
+            #simulation.reporters.append(app.StateDataReporter("simulation_{tag}_{timestep}_{temperature}.log".format(**locals()).replace(" ",""), 100, step=True, totalEnergy=True,
+            #potentialEnergy=True, temperature=True, progress=True, remainingTime=True,
+            #speed=True, totalSteps=steps, separator=','))
+
+            start = datetime.datetime.now()
+            simulation.step(steps)
+            end = datetime.datetime.now()
+            print("%s, %s, %s, %d, %.0f" % (forcefield_configuration, platform_name, boundary, num_molecules, (end-start).total_seconds()))
